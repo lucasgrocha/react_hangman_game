adeec839eadfd699f89c34fbd93d39a5088e7d8e
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-05-16 01:09:34 -0300

    add lives counter style

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ const StyledBoard = styled.div`
   background-color: white;
   border-radius: 10px;
   padding: 10px;
-  width: 75vw;
   margin: auto;
   display: flex;
   justify-content: center;
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,6 +1,16 @@
 import React, { useState, Fragment } from 'react'
 import Letters from './Letters'
 import { Container, Row, Col } from 'react-bootstrap'
+import styled from 'styled-components'
+
+const StyledLivesCounter = styled.div`
+  
+`
+
+const StyledLetters = styled.div`
+  text-align: center;
+  width: 75vw;
+`
 
 const Game = props => {
   const [lives, setLives] = useState(4)
@@ -13,15 +23,19 @@ const Game = props => {
     <Container>
       <Row>
         <Col sm={12}>
-          <h1>{lives}</h1>
+          <StyledLivesCounter>
+            <span>{lives}</span>
+          </StyledLivesCounter>
         </Col>
       </Row>
       <Row>
         <Col sm={12}>
-          <Letters
-            word={props.word}
-            lives={lives}
-            changeLives={handleLives} />
+          <StyledLetters>
+            <Letters
+              word={props.word}
+              lives={lives}
+              changeLives={handleLives} />
+          </StyledLetters>
         </Col>
       </Row>
     </Container>
