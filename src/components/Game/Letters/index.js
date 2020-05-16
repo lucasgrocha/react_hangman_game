1f99c5c0f4eddedde717c559bc2b21cf81d54c55
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-05-15 21:02:41 -0300

    display corrected letters along the game

--- src/components/Game/Letters/index.js
+++ src/components/Game/Letters/index.js
@@ -14,14 +14,15 @@ const LetterWrapper = styled.div`
 `
 
 const Letters = props => {
-  const [word, setWord] = useState(_.toUpper(props.word))
-  const [randomLetters, setRandomLetters] = useState([])
+  const [word] = useState(_.toUpper(props.word))
+  const [correctedWord, setCorrectedWord] = useState([])
   const [mixedRandomLetters, setMixedRandomLetters] = useState([])
 
   useEffect(() => {
     let wordAndLetters = _.toUpper(word + 'ZYXXX')
     wordAndLetters = removeDuplicates(wordAndLetters)
-    setRandomLetters(lettersFactory(wordAndLetters))
+
+    setCorrectedWord(lettersFactory(word))
     setMixedRandomLetters(lettersFactory(_.shuffle(wordAndLetters.split('')).join('')))
   }, [word])
 
@@ -43,16 +44,22 @@ const Letters = props => {
 
   const handleClickedLetter = (letter, randomLetterIndex) => {
     const tmpMixed = [...mixedRandomLetters]
+    const tmpCorrect = [...correctedWord]
     if (word.split('').includes(letter)) {
       for (let index of getIndices(mixedRandomLetters, letter)) {
         tmpMixed[index].status = 'correct'
+      }
+
+      for (let index of getIndices(correctedWord, letter)) {
+        tmpCorrect[index].status = 'correct'
       }
     } else {
       tmpMixed[randomLetterIndex].status = 'incorrect'
     }
     console.clear()
     console.log("Word: " + word)
-    console.table(tmpMixed)
+    console.table(tmpCorrect)
+    setCorrectedWord(tmpCorrect)
     setMixedRandomLetters(tmpMixed)
   }
 
@@ -64,7 +71,7 @@ const Letters = props => {
     <Container>
       <Row>
         <Col sm={12}>
-          <Phrase letters={mixedRandomLetters} />
+          <Phrase letters={correctedWord} />
         </Col>
       </Row>
       <Row>
